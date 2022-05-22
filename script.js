fef454aa2362c3ba300eeecee383379e35265290
jakovid <jacobk.dowdle@gmail.com>
2022-05-22 22:01:44 +0800

    added function to add moves that can't be overwritten and switch game symbols

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
-function selection() {
-    this.style.backgroundColor = "black";
-    this.innerHTML = "X";
+//variable
+const gameSquares = document.querySelectorAll(".gameSquare");
+let turn = "X";
+
+//event listeners
+gameSquares.forEach(div => {
+    div.addEventListener('click', userPick)
+})
+
+//functions
+function userPick(e) {
+
+    let playable = true;
+
+    if (this.innerHTML === " "){
+        playable = true;
+    } else {
+        playable = false;
+    }
+    
+    if (e.type === "click" && playable) {
+        this.innerHTML = turn;
+    }
+
+    if (!playable){
+    } else if (turn === "X") {
+        turn = "O";
+    } else {
+        turn = "X";
+    }
 }
 
 function reset() {
-    let gameSquares = document.querySelectorAll(".gameSquare");
-    gameSquares.forEach((div) => div.innerHTML = "");
+    gameSquares.forEach((div) => div.innerHTML = " ");
     gameSquares.forEach((div) => div.style.backgroundColor = 'white');
 }
