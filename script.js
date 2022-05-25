80a68a90fa1d74d2b6df5031703c12dd838212f1
Jacob Dowdle <jacobk.dowdle@gmail.com>
2022-05-25 10:51:58 +0800

    first attempt at adding a winCheck function

--- script.js
+++ script.js
@@ -34,6 +34,7 @@ function userPick(e) {
     } else {
         turn = "X";
     }
+    winCheck();
 }
 
 function reset() {
@@ -44,4 +45,26 @@ function reset() {
 function symbolSelect() {
     reset();
     turn = `${this.value}`;
+}
+
+function winCheck(){
+    let r1c1 = document.getElementById("1a").innerHTML;
+    let r1c2 = document.getElementById("2a").innerHTML;
+    let r1c3 = document.getElementById("3a").innerHTML;
+    let r2c1 = document.getElementById("1b").innerHTML;
+    let r2c2 = document.getElementById("2b").innerHTML;
+    let r2c3 = document.getElementById("3b").innerHTML;
+    let r3c1 = document.getElementById("1c").innerHTML;
+    let r3c2 = document.getElementById("2c").innerHTML;
+    let r3c3 = document.getElementById("3c").innerHTML;
+    let g = [r1c1,r1c2,r1c3,r2c1,r2c2,r2c3,r3c1,r3c2,r3c3];
+
+    if
+    ((g[0] === (g[1] && g[2]) || g[0] === (g[3] && g[6])) && g[0] != " "){
+        console.log("win!");
+    } 
+    // else if 
+    // ((g[8] === (g[6] && g[7]) || g[8] === (g[2] && g[5])) && g[8] != " "){
+    //     console.log("win!");
+    // }
 }
